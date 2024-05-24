7708cff62f5381b985b4a849fd8b303722e39de5
jsbroden <julia-broden@t-online.de>
2024-05-24 15:40:45 +0200

    files for final exercise

---
+++ model.py
@@ -0,0 +1,39 @@
+import torch
+from torch import nn
+
+class MyAwesomeModel(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1)
+        self.dropout = nn.Dropout(0.5)
+        self.fc1 = nn.Linear(128, 10)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = torch.relu(self.conv1(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv2(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.relu(self.conv3(x))
+        x = torch.max_pool2d(x, 2, 2)
+        x = torch.flatten(x, 1)
+        x = self.dropout(x)
+        x = self.fc1(x)
+
+        return x
+
+# test to instantiate a model
+if __name__ == "__main__": 
+    '''
+    checks if the script is being run directly (not being imported as a module). 
+    If it is, the code within this block will be executed.
+    '''
+    model = MyAwesomeModel()
+    print(f"Model architecture: {model}")
+    print(f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
+
+    dummy_input = torch.randn(1, 1, 28, 28)
+    output = model(dummy_input)
+    print(f"Output shape: {output.shape}")
